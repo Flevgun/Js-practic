19532a0903a4bcff96855e1b19abcc2f1479025e
Flevgun <sansan0851.sk@gmail.com>
2023-01-21 08:40:39 +0500

    add timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import {tabs} from './modules/tabs.js';
+import {timer} from './modules/timer.js'
 ('use strict');
 window.addEventListener('DOMContentLoaded', () => {
 
     tabs();
+    timer();
 
 })
