21209947bd1044cddde9695b1e9c04c25f68f947
Flevgun <sansan0851.sk@gmail.com>
2023-01-30 17:59:22 +0500

    add post request

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-import {tabs} from './modules/tabs.js';
-import {timer} from './modules/timer.js';
-import { modal } from './modules/modal.js';
-('use strict');
-window.addEventListener('DOMContentLoaded', () => {
-
-    tabs();
-    timer();
-    modal();
-
-})
+import { tabs } from "./modules/tabs.js";
+import { timer } from "./modules/timer.js";
+import { modal } from "./modules/modal.js";
+import { form } from "./modules/form.js";
+("use strict");
+window.addEventListener("DOMContentLoaded", () => {
+  tabs();
+  timer();
+  modal();
+  form();
+});
